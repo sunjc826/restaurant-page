ad1787e7d24eceec5cacfb942980cb073ada4b61
Sun Jia Cheng <sunjc826@gmail.com>
2020-09-20 22:21:17 +0800

    Add tab functionality

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import loadContact from "./contact.js";
 import loadMenu from "./menu.js";
 
 const body = document.querySelector("body");
+const tabPanel = document.createElement("div");
+tabPanel.setAttribute("style", 
+    "display: flex;\
+    flex-direction: row;");
 const tabs = [];
 tabs.push(createTab("Homepage", loadInit));
 tabs.push(createTab("Contact Us", loadContact));
 tabs.push(createTab("Menu Selection", loadMenu));
-
-const content = document.querySelector("#content");
 for (let tab of tabs) {
-    body.insertBefore(tab, content);
+    tabPanel.appendChild(tab);
 }
 
+const content = document.querySelector("#content");
+body.insertBefore(tabPanel, content);
+
+let selectedTab = tabs[0];
+toggleTab(selectedTab);
 loadInit(content);
 
 function createTab(tabName, fn) {
@@ -20,10 +27,20 @@ function createTab(tabName, fn) {
     tab.textContent = tabName;
     tab.setAttribute("style", "text-align: left");
     tab.addEventListener("click", tabEvent);
+    tab.classList.add("tab");
+    tab.classList.add("tab-deselected");
 
     function tabEvent(event) {
+        toggleTab(selectedTab);
+        toggleTab(this);
+        selectedTab = this;
         fn(content);
     }
 
     return tab;
+}
+
+function toggleTab(tab) {
+    tab.classList.toggle("tab-deselected")
+    tab.classList.toggle("tab-selected");
 }
